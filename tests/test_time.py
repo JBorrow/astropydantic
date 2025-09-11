0d15728de5f72cf44813d2d596df80eb3fba3b0f
Maximilian Linhoff <maximilian.linhoff@cta-observatory.org>
2025-09-11 12:02:30 +0200

    Fix time serialization using .value in case of datetime, allow setting scale

--- tests/test_time.py
+++ tests/test_time.py
@@ -26,10 +26,10 @@ def test_unit_string():
     TestModel.model_validate_json(json)
 
 
-def test_unit_datetime():
+def test_unit_datetime(monkeypatch):
     import astropydantic
 
-    astropydantic.TIME_OUTPUT_FORMAT = "datetime"
+    monkeypatch.setattr(astropydantic, "TIME_OUTPUT_FORMAT", "datetime")
 
     class TestModel(BaseModel):
         x: AstroPydanticTime
@@ -53,6 +53,36 @@ def test_unit_datetime():
     assert isinstance(serialized["x"], datetime.datetime)
 
 
+def test_unit_mjd(monkeypatch):
+    import astropydantic
+
+    monkeypatch.setattr(astropydantic, "TIME_OUTPUT_FORMAT", "mjd")
+
+    class TestModel(BaseModel):
+        t: AstroPydanticTime
+
+    now = t.Time.now()
+    m = TestModel(t=now)
+    serialized = m.model_dump()
+    assert serialized["t"] == now.utc.mjd
+
+
+def test_scale(monkeypatch):
+    import astropydantic
+
+    monkeypatch.setattr(astropydantic, "TIME_OUTPUT_SCALE", "tai")
+
+    class TestModel(BaseModel):
+        t: AstroPydanticTime
+
+    now = t.Time.now()
+    m = TestModel(t=now.utc)
+    serialized = m.model_dump()
+
+    now.precision = 9
+    assert serialized["t"] == now.tai.isot
+
+
 def test_unit_disallowed():
     import astropydantic
 
@@ -67,4 +97,3 @@ def test_unit_disallowed():
 
     with pytest.raises(ValueError):
         m.model_dump()
-
